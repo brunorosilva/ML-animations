b24851e6819c68d9fb2cc14a6c5a55254ac63a63
Bruno Rodrigues Silva <b.rosilva1@gmail.com>
2020-06-02 00:12:17 -0300

    Linear Regression
    Lots of changes

--- MachineLearning/linear_regression_2d.py
+++ MachineLearning/linear_regression_2d.py
@@ -5,18 +5,14 @@ import tensorflow as tf
 from sklearn.datasets import make_regression
 
 class LinearRegressionAnimation():
-
-
-    #tf.enable_eager_execution()
     """
     This object creates a mp4, GIF or HTML5 video of the Linear Regression Algorithm
-    """
+    """        
         
-        
-    def __init__(self, train_X=np.nan, train_Y=np.nan, W = tf.Variable(np.random.randn(), name="weight"), b = tf.Variable(np.random.randn(), name="bias"), alpha=0.1, size=100):
+    def __init__(self, train_X=np.nan, train_Y=np.nan, W = tf.Variable(np.random.randn(), name="weight"), b = tf.Variable(np.random.randn(), name="bias"), alpha=0.01, size=100, noise=2):
         if np.isnan(train_X):
             print("Taking random X")
-            self.train_X, self.train_Y = make_regression(100, 1, noise=2)
+            self.train_X, self.train_Y = make_regression(size, 1, noise=noise)
         else:
             self.train_X = train_X
             self.train_Y = train_Y
@@ -27,8 +23,7 @@ class LinearRegressionAnimation():
         self.b = b
         self.alpha = alpha
         self.n_samples = size
-
-        
+    
     def animate(self,
         frames=20,
         line_color='black', 
@@ -56,25 +51,24 @@ class LinearRegressionAnimation():
                 for (x, y) in zip(self.train_X, self.train_Y):
                     sess.run(optimizer, feed_dict={self.X: x, self.Y: y})
 
-                if epoch % 10 == 0:
+                if (epoch + 1) % 10 == 0:
                     c = sess.run(cost, feed_dict={self.X:self.train_X, self.Y:self.train_Y})
                     print("Epoch:", '%04d' % (epoch+1), "cost=", "{:.9f}".format(c), \
                         "W=", sess.run(self.W), "b=", sess.run(self.b))
                     
-                    ax.scatter(self.train_X, self.train_Y)
-                    ax.plot(self.train_X, sess.run(self.W) * self.train_X + sess.run(self.b))
+                    ax.scatter(self.train_X, self.train_Y, c=dot_color)
+                    ax.plot(self.train_X, sess.run(self.W) * self.train_X + sess.run(self.b), c=line_color)
                     cam.snap()
 
             anim = cam.animate()
-            anim.save("uhuhuh.mp4")
-
+            
             print("Optimization Finished!")
             training_cost = sess.run(cost, feed_dict={self.X: self.train_X, self.Y: self.train_Y})
             print("Training cost=", training_cost, "W=", sess.run(self.W), "b=", sess.run(self.b), '\n')
 
-    def show_anim(self):
-        pass
+        return anim
 
 
-an = LinearRegressionAnimation()
-an.animate()
+ling = LinearRegressionAnimation()
+animation = ling.animate()
+animation.save("Linear_Regression.mp4")
