e912e280388bc8c557610e9615623b237fcb6902
Bruno Rodrigues Silva <b.rosilva1@gmail.com>
2020-06-29 11:24:59 -0300

    Add files via upload

--- MachineLearning/svc_animation.py
+++ MachineLearning/svc_animation.py
@@ -3,17 +3,17 @@ import matplotlib.pyplot as plt
 from celluloid import Camera
 from sklearn.svm import SVC
 
+# Adaptado do material de  R. Jordan Crouser do Smith College (2016)
 def anim_svc(svc, X, y, h=0.05, pad=0.25):
     x_min, x_max = X[:, 0].min()-pad, X[:, 0].max()+pad
     y_min, y_max = X[:, 1].min()-pad, X[:, 1].max()+pad
     xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
     Z = svc.predict(np.c_[xx.ravel(), yy.ravel()])
-    print(Z)
     Z = Z.reshape(xx.shape)
     
     plt.contourf(xx, yy, Z, cmap=plt.cm.Paired, alpha=0.2)
 
-    plt.scatter(X[:,0], X[:,1], s=70, c=y, cmap=mpl.cm.Paired)
+    plt.scatter(X[:,0], X[:,1], s=70, c=y, cmap=plt.cm.Paired)
     # Mostra os vetores de suporte com um 'x'
     sv = svc.support_vectors_
     plt.scatter(sv[:,0], sv[:,1], c='k', marker='x', s=100, linewidths='1')
@@ -37,6 +37,5 @@ for c in [0.0001, 0.001, 0.01, 0.1, 1, 10, 100, 1000, 10000]:
   model = SVC(c).fit(np.array([x, y]).T, z)
   anim = anim_svc(model, np.array([x, y]).T, z)
 
-from IPython.display import HTML
 animation = anim.animate()
-HTML(animation.to_html5_video())
+animation.save("SVC.gif")
